4769de37e450728ccf33ce131cac3e5962f75bcb
Jamie Schlessinger <james.schlessinger@gmail.com>
2014-10-16 20:45:50 -0700

    Hopefully fix some weirdness with directionId

--- app/assets/javascripts/backbone/collections/departures.js
+++ app/assets/javascripts/backbone/collections/departures.js
@@ -1,7 +1,9 @@
 Transit.Collections.Departures = Backbone.Collection.extend({
   initialize: function(agencyId, routeId, stopId, directionId) {
     this.url = "/agencies/" + agencyId + "/routes/" + routeId
-    if (typeof directionId != 'undefined') { this.url += ("/directions/" + directionId); };
+    if (typeof directionId != 'undefined' && directionId.length !== 0) {
+      this.url += ("/directions/" + directionId);
+    }
     this.url += ("/stops/" + stopId);
     this.url += "/departures";
   },
